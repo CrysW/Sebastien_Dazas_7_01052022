1ad0936369f36af48362879ebcb9c70c5e1e49fb
Sebastien Dazas <dazas.sebastien@gmail.com>
2022-05-08 19:29:02 +0200

    Mise à jour de la route pour modifier une publication

--- backend/controllers/publication.js
+++ backend/controllers/publication.js
@@ -139,31 +139,153 @@ exports.seeAllPublications = function (req, res, next) {
 exports.updatePublication = function (req, res, next) {
   // Requête SQL pour récupérer les données de la publication dans la base de données
   mysqlConnection.query(
-    `SELECT idPublication, users.idUser, firstName, lastName, profilePicture, CAST(publicationDate AS CHAR) AS publicationDate, publicationPicture, publicationContent FROM users INNER JOIN publications ON users.idUser = publications.idUser WHERE idPublication=${req.params.id}`,
+    `SELECT idPublication, users.idUser, firstName, lastName, profilePicture, CAST(publicationDate AS CHAR) AS publicationDate, publicationPicture, publicationContent FROM users INNER JOIN publications ON users.idUser = publications.idUser WHERE idPublication = "${req.params.id}"`,
     function (error, results, fields) {
       if (error) {
         res
           .status(400)
           .json({ message: "Une erreur est survenue ! 😅", error });
       } else {
-        // Récupération de la photo à ne pas sauvegarder côté serveur
-        const pictureToDelete =
-          results[0].publicationPicture.split("/images")[1];
-        // CAS 1 : Il y a une image et un message à mettre à jour
-        if (req.file && req.body.publication) {
-          // Récupération des données contenu dans req.body.publication
-          const dataReqBodyPublication = JSON.parse(req.body.publication);
-          // Récupération de l'image dans req.file.filename
-          const dataReqFile = `${req.protocol}://${req.get("host")}/images/${
-            req.file.filename
-          }`;
-          if (
-            dataReqBodyPublication.publicationContent.length >= 10 &&
-            dataReqBodyPublication.publicationContent.length <= 280
-          ) {
+        // SI LA PHOTO A RECUPERER COTE SERVEUR N'EXISTE PAS
+        if (results[0].publicationPicture === null) {
+          // CAS 1 : Il y a une image et un message à mettre à jour
+          if (req.file && req.body.publication) {
+            // Récupération des données contenues dans req.body.publication
+            const dataReqBodyPublication = JSON.parse(req.body.publication);
+            // Récupération de l'image dans req.file.filename
+            const dataReqFile = `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`;
+            if (
+              dataReqBodyPublication.publicationContent.length >= 10 &&
+              dataReqBodyPublication.publicationContent.length <= 280
+            ) {
+              // Requête SQL pour mettre à jour les données dans la base de données
+              mysqlConnection.query(
+                `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}", publicationPicture = "${dataReqFile}" WHERE publications.idPublication = "${req.params.id}";`,
+                function (error, results, fields) {
+                  if (error) {
+                    res
+                      .status(400)
+                      .json({ message: "Une erreur est survenue ! 😅", error });
+                  } else {
+                    res.status(200).json({
+                      message:
+                        "L'image et le message ont été mises à jour dans la base de données ! 🥳",
+                    });
+                  }
+                }
+              );
+            } else {
+              // Récupération de l'image à ne pas sauvegarder côté serveur
+              const pictureToDelete = dataReqFile.split("/images")[1];
+              console.log(pictureToDelete);
+              // Suppression de l'image dans le dossier 'images' du serveur
+              fs.removeSync(`images/${pictureToDelete}`);
+              // Réponse du serveur
+              res.status(400).json({
+                message:
+                  "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
+              });
+            }
+          }
+          // CAS 2 : Il y a un message à mettre à jour uniquement
+          else if (!req.file && req.body.publication) {
+            // Récupération des données contenues dans req.body.publication
+            const dataReqBodyPublication = JSON.parse(req.body.publication);
+            if (
+              dataReqBodyPublication.publicationContent.length >= 10 &&
+              dataReqBodyPublication.publicationContent.length <= 280
+            ) {
+              // Requête SQL pour mettre à jour les données dans la base de données
+              mysqlConnection.query(
+                `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}" WHERE publications.idPublication = "${req.params.id}";`,
+                function (error, results, fields) {
+                  if (error) {
+                    res
+                      .status(400)
+                      .json({ message: "Une erreur est survenue ! 😅", error });
+                  } else {
+                    res.status(200).json({
+                      message:
+                        "Le message a été mises à jour dans la base de données ! 🥳",
+                    });
+                  }
+                }
+              );
+            } else {
+              res.status(400).json({
+                message:
+                  "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
+              });
+            }
+          }
+          // CAS 3 : Il n'y a pas d'image ou de message à mettre à jour
+          else if (!req.file && !req.body.publication) {
+            res.status(200).json({
+              message: "Aucunes données n'a besoin d'être mises à jour ! 🥳",
+            });
+          }
+        }
+        // SINON SI LA PHOTO A RECUPERE COTE SERVEUR EXISTE
+        else {
+          // Récupération de la photo à ne pas sauvegarder côté serveur
+          const pictureToDelete =
+            results[0].publicationPicture.split("/images")[1];
+          // CAS 1 : Il y a une image et un message à mettre à jour
+          if (req.file && req.body.publication) {
+            // Récupération des données contenues dans req.body.publication
+            const dataReqBodyPublication = JSON.parse(req.body.publication);
+            // Récupération de l'image dans req.file.filename
+            const dataReqFile = `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`;
+            if (
+              dataReqBodyPublication.publicationContent.length >= 10 &&
+              dataReqBodyPublication.publicationContent.length <= 280
+            ) {
+              // Requête SQL pour mettre à jour les données dans la base de données
+              mysqlConnection.query(
+                `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}", publicationPicture = "${dataReqFile}" WHERE publications.idPublication = "${req.params.id}";`,
+                function (error, results, fields) {
+                  if (error) {
+                    res
+                      .status(400)
+                      .json({ message: "Une erreur est survenue ! 😅", error });
+                  } else {
+                    res.status(200).json({
+                      message:
+                        "L'image et le message ont été mises à jour dans la base de données ! 🥳",
+                    });
+                    // Suppression de l'image dans le dossier 'images' du serveur
+                    fs.removeSync(`images/${pictureToDelete}`);
+                  }
+                }
+              );
+            } else {
+              // Récupération du nom de l'image à ne pas sauvegarder côté serveur
+              let pictureNotToSave = `${req.protocol}://${req.get(
+                "host"
+              )}/images/${req.file.filename}`;
+              pictureNotToSave = pictureNotToSave.split("/")[4];
+              // Suppression de l'image dans le dossier 'images'
+              fs.removeSync(`images/${pictureNotToSave}`);
+              // Envoi de la réponse du serveur
+              res.status(400).json({
+                message:
+                  "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
+              });
+            }
+          }
+          // CAS 2 : Il y a une image à mettre à jour uniquement
+          else if (req.file && !req.body.publication) {
+            // Récupération de l'image dans req.file.filename
+            const dataReqFile = `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`;
             // Requête SQL pour mettre à jour les données dans la base de données
             mysqlConnection.query(
-              `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}", publicationPicture = "${dataReqFile}" WHERE publications.idPublication = "${req.params.id}";`,
+              `UPDATE publications SET publicationPicture = "${dataReqFile}" WHERE publications.idPublication = "${req.params.id}";`,
               function (error, results, fields) {
                 if (error) {
                   res
@@ -172,89 +294,51 @@ exports.updatePublication = function (req, res, next) {
                 } else {
                   res.status(200).json({
                     message:
-                      "L'image et le message ont été mises à jour dans la base de données ! 🥳",
+                      "L'image a été mises à jour dans la base de données ! 🥳",
                   });
                   // Suppression de l'image dans le dossier 'images' du serveur
                   fs.removeSync(`images/${pictureToDelete}`);
                 }
               }
             );
-          } else {
-            // Récupération du nom de l'image à ne pas sauvegarder côté serveur
-            let pictureNotToSave = `${req.protocol}://${req.get(
-              "host"
-            )}/images/${req.file.filename}`;
-            pictureNotToSave = pictureNotToSave.split("/")[4];
-            // Suppression de l'image dans le dossier 'images'
-            fs.removeSync(`images/${pictureNotToSave}`);
-            // Envoi de la réponse du serveur
-            res.status(400).json({
-              message:
-                "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
-            });
           }
-        }
-        // CAS 2 : Il y a une image à mettre à jour uniquement
-        else if (req.file && !req.body.publication) {
-          // Récupération de l'image dans req.file.filename
-          const dataReqFile = `${req.protocol}://${req.get("host")}/images/${
-            req.file.filename
-          }`;
-          // Requête SQL pour mettre à jour les données dans la base de données
-          mysqlConnection.query(
-            `UPDATE publications SET publicationPicture = "${dataReqFile}" WHERE publications.idPublication = "${req.params.id}";`,
-            function (error, results, fields) {
-              if (error) {
-                res
-                  .status(400)
-                  .json({ message: "Une erreur est survenue ! 😅", error });
-              } else {
-                res.status(200).json({
-                  message:
-                    "L'image a été mises à jour dans la base de données ! 🥳",
-                });
-                // Suppression de l'image dans le dossier 'images' du serveur
-                fs.removeSync(`images/${pictureToDelete}`);
-              }
-            }
-          );
-        }
-        // CAS 3 : Il y a un message à mettre à jour uniquement
-        else if (!req.file && req.body.publication) {
-          // Récupération des données contenu dans req.body.publication
-          const dataReqBodyPublication = JSON.parse(req.body.publication);
-          if (
-            dataReqBodyPublication.publicationContent.length >= 10 &&
-            dataReqBodyPublication.publicationContent.length <= 280
-          ) {
-            // Requête SQL pour mettre à jour les données dans la base de données
-            mysqlConnection.query(
-              `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}" WHERE publications.idPublication = "${req.params.id}";`,
-              function (error, results, fields) {
-                if (error) {
-                  res
-                    .status(400)
-                    .json({ message: "Une erreur est survenue ! 😅", error });
-                } else {
-                  res.status(200).json({
-                    message:
-                      "Le message a été mises à jour dans la base de données ! 🥳",
-                  });
+          // CAS 3 : Il y a un message à mettre à jour uniquement
+          else if (!req.file && req.body.publication) {
+            // Récupération des données contenues dans req.body.publication
+            const dataReqBodyPublication = JSON.parse(req.body.publication);
+            if (
+              dataReqBodyPublication.publicationContent.length >= 10 &&
+              dataReqBodyPublication.publicationContent.length <= 280
+            ) {
+              // Requête SQL pour mettre à jour les données dans la base de données
+              mysqlConnection.query(
+                `UPDATE publications SET publicationContent = "${dataReqBodyPublication.publicationContent}" WHERE publications.idPublication = "${req.params.id}";`,
+                function (error, results, fields) {
+                  if (error) {
+                    res
+                      .status(400)
+                      .json({ message: "Une erreur est survenue ! 😅", error });
+                  } else {
+                    res.status(200).json({
+                      message:
+                        "Le message a été mises à jour dans la base de données ! 🥳",
+                    });
+                  }
                 }
-              }
-            );
-          } else {
-            res.status(400).json({
-              message:
-                "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
+              );
+            } else {
+              res.status(400).json({
+                message:
+                  "La publication nécessite un message compris entre 10 et 280 caractères ! 😅",
+              });
+            }
+          }
+          // CAS 4 : Il n'y a pas d'image ou de message à mettre à jour
+          else if (!req.file && !req.body.publication) {
+            res.status(200).json({
+              message: "Aucunes données n'a besoin d'être mises à jour ! 🥳",
             });
           }
-        }
-        // CAS 4 : Il n'y a pas d'image ou de message à mettre à jour
-        else if (!req.file && !req.body.publication) {
-          res.status(200).json({
-            message: "Aucunes données n'a besoin d'être mises à jour ! 🥳",
-          });
         }
       }
     }
