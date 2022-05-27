081ba7fbe6facd59ffb7cff06d731f29238f84da
Sebastien Dazas <dazas.sebastien@gmail.com>
2022-05-27 11:13:55 +0200

    Mise à jour de 'HeaderComponent'

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,46 +1,50 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark perso-navbar">
-    <!-- Logo et nom de l'entreprise -->
-    <img
-      class="navbar-brand"
-      src="../assets/logo-groupomania.png"
-      alt="logo"
-      width=""
-      height="50px"
-    />
-    <!-- Bouton 'hamburger' pour la navigation sur les écrans plus petits -->
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <!-- Barre de navigation -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item mx-3">
-          <router-link class="nav-link text-white" to="/profile"
-            >Profile</router-link
-          >
-        </li>
-        <li class="nav-item mx-3">
-          <router-link class="nav-link text-white" to="/homepage"
-            >Accueil</router-link
-          >
-        </li>
-        <li class="nav-item mx-3">
-          <a v-on:click="logout()" class="nav-link text-white" to="/"
-            >Deconnexion</a
-          >
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark perso-navbar">
+      <!-- Logo et nom de l'entreprise -->
+      <img
+        class="navbar-brand"
+        src="../assets/logo-groupomania.png"
+        alt="logo"
+        width=""
+        height="50px"
+      />
+      <!-- Bouton 'hamburger' pour la navigation sur les écrans plus petits -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Barre de navigation -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-3">
+            <router-link class="nav-link text-white" to="/profile"
+              >Profile</router-link
+            >
+          </li>
+          <li class="nav-item mx-3">
+            <router-link class="nav-link text-white" to="/homepage"
+              >Accueil</router-link
+            >
+          </li>
+          <li class="nav-item mx-3">
+            <a v-on:click="logout()" class="nav-link text-white" to="/"
+              >Deconnexion</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Liseret -->
+    <div class="liseret"></div>
+  </div>
 </template>
 
 <script>
@@ -80,5 +84,11 @@ a {
 // Style pour le survole des liens dans la barre de navigation
 .nav-link:hover {
   color: #d1515a !important;
+}
+// Style du liseret
+.liseret {
+  width: 100%;
+  height: 10px;
+  background-color: #d1515a;
 }
 </style>
